24bb18ded2e4fc9928b671385040a6a17417e583
uche <travzki@gmail.com>
2022-04-07 23:45:06 +0000

    test.sh updated

--- test.sh
+++ test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+declare -a directories=(front-end lotto-api lottodraw-api prize-api)
+for dir in "${directories[@]}"; do
+cd ${dir}
 sudo apt update && sudo apt install python3 python3-pip python3-venv -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r test_requirements.txt
-python3 -m pytest --cov=application --cov-report=html
-
-declare -a directories=(front-end lotto-api lottodraw-api prize-api)
-for dir in "${directories[@]}"; do
-cd ${dir}
 python3 -m pytest -p no:warnings --cov=application --cov-report=html
 cd ..
 done
