07fca5c60ae2390b3dfb24cfe6b0aeb919367005
uche <travzki@gmail.com>
2022-04-07 09:52:10 +0000

    bug fix

--- db/create.sql
+++ db/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS results (
     pk INT PRIMARY KEY AUTO_INCREMENT,
     magicNumber VARCHAR(10) NOT NULL,
-    lottoDraw INT(10) NOT NULL,
+    lottoDraw VARCHAR(20) NOT NULL,
     prize INT(10) NOT NULL
 );
