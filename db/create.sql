1081711494f144f8571fe87ded2e4be91d6c3f82
uche <travzki@gmail.com>
2022-04-07 09:36:49 +0000

    db fix

--- db/create.sql
+++ db/create.sql
@@ -1,7 +1,6 @@
-CREATE DATABASE IF NOT EXISTS lotto;
-USE lotto;
 CREATE TABLE IF NOT EXISTS results (
     pk INT PRIMARY KEY AUTO_INCREMENT,
+    magicNumber INT(10) NOT NULL,
     lottoDraw INT(10) NOT NULL,
     prize VARCHAR(10) NOT NULL
 );
