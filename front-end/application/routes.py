3665f9381f6f62a7290da90fcb086d4a4cf74b4c
uche <travzki@gmail.com>
2022-04-07 09:48:35 +0000

    bug fix

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -8,7 +8,7 @@ def index():
     magicNumber = requests.get('http://lotto-api:5000/get-magicNumber')
     lottoDraw = requests.get('http://lottodraw-api:5000/get-draw')
     prize = requests.post('http://prize-api:5000/prize', json=magicNumber.json())
-    db.session.add(Results(magicNumber=magicNumber.json()["magicNumber"], lottoDraw=lottoDraw.json()["lottoDraw"], prize=prize.json()["prize"]))
+    db.session.add(Results(magicNumber=magicNumber.json()["magicNumber"], lottoDraw=','.join(str(n) for n in lottoDraw.json()["lottoDraw"]), prize=prize.json()["prize"]))
     db.session.commit()
     results = Results.query.all()
 
