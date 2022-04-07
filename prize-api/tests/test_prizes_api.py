bca995731a68dccd333b619933507d7551b58b32
uche <travzki@gmail.com>
2022-04-07 23:35:33 +0000

    files added

--- prize-api/tests/test_prizes_api.py
+++ prize-api/tests/test_prizes_api.py
@@ -12,22 +12,22 @@ class TestView(TestBase):
         self.assert200(response)
         self.assertIn(b'100', response.data)
 
-    def test_get_prize_1(self):
+    def test_get_prize_2(self):
         response = self.client.post(url_for('prize'), json={'magicNumber':'two'})
         self.assert200(response)
         self.assertIn(b'200', response.data)
 
-    def test_get_prize_1(self):
+    def test_get_prize_3(self):
         response = self.client.post(url_for('prize'), json={'magicNumber':'three'})
         self.assert200(response)
         self.assertIn(b'300', response.data)
 
-    def test_get_prize_1(self):
+    def test_get_prize_4(self):
         response = self.client.post(url_for('prize'), json={'magicNumber':'four'})
         self.assert200(response)
         self.assertIn(b'400', response.data)
 
-    def test_get_prize_1(self):
+    def test_get_prize_5(self):
         response = self.client.post(url_for('prize'), json={'magicNumber':'five'})
         self.assert200(response)
         self.assertIn(b'500', response.data)
